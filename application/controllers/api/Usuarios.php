0ac369531fc72c5d272a0a089370f7f255246715
amondocorre01 <amondocorre@capressocafe.com>
2022-09-02 19:27:30 -0400

    z

--- application/controllers/api/Usuarios.php
+++ application/controllers/api/Usuarios.php
@@ -127,14 +127,14 @@
         public function menu() {
 
             $id = $this->input->post('id');
-           
+           $data['id']=$id;
             $data['menu'] = $this->main->getListSelect('VENTAS_ACCESO va', 'ID_VENTAS_ACCESO, NOMBRE, NIVEL_SUPERIOR,NUMERO_ORDEN, ( 
                 SELECT ID_VENTAS_ACCESO 
                 FROM VENTAS_USUARIOS_ACCESO vua 
                 WHERE vua.ID_USUARIO = '.$id.' AND 
                 vua.ID_VENTAS_ACCESO = va.ID_VENTAS_ACCESO
             ) AS ACCEDE');
-
+            //var_dump($data);
             echo $this->load->view('usuario/body/permisos', $data, TRUE);
             
         }
