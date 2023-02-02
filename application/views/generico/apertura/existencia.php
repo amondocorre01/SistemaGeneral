2bbdcaac33a62b58b571fb5eec76a70178b43f3f
John Aleman <john.aleman.bo@gmail.com>
2023-02-02 15:34:34 -0400

    .

--- application/views/generico/apertura/existencia.php
+++ application/views/generico/apertura/existencia.php
@@ -3,134 +3,116 @@
   button {
      margin-top: 0px;
   }
+
+  .table-classic {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.table-classic td, .table-classic th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+.card-header {
+    padding: 0.25rem 1.25rem 0.15rem;
+}
+
+.table-classic tr:nth-child(even){background-color: #f2f2f2;}
+
+.table-classic tr:hover {background-color: #ddd;}
+
+.table-classic th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #b30035;
+  color: black;
+}
 </style>
-<div id="accordion">
-  <?php foreach ($existencia as $value) : ?>
-    <div class="card" style="background-color: rgb(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?> )">
-      <div class="card-header" id="heading<?=$value->ID_CATEGORIA?>">
-        <h5 class="mb-0">
-        <?=$value->CATEGORIA?>
 
-          <button class="btn btn-danger float-right btn-xs" data-toggle="collapse" data-target="#collapse<?=$value->ID_CATEGORIA?>" aria-expanded="true" aria-controls="collapseOne">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Pedidos</a>
+        <?=form_button('agregar', '<span style="font-size:1.5rem" class="las la-save la-2x"></span>', ['class'=>'btn btn-danger btn-xs float-right', 'onclick'=>'guardarDeclaracion()']);?>
+    </nav>
+
+<br>
+<div class="card" id="serializeExample">
+  <form method="post">
+    <div id="accordion">
+      <?php foreach ($existencia as $value) : ?>
+        <div class="card" style="background-color: rgb(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?> )">
+          <div class="card-header" id="heading<?=$value->ID_CATEGORIA?>">
+            <h6 >
+            <?=$value->CATEGORIA?>
+
+              <button class="btn btn-danger float-right btn-xs" data-toggle="collapse" data-target="#collapse<?=$value->ID_CATEGORIA?>" aria-expanded="true" aria-controls="collapseOne">
+                <i class="las la-plus" ></i>
+              </button>
+            </h6>
+          </div>
+
+          <div id="collapse<?=$value->ID_CATEGORIA?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body" style="background-color:white">
+
+            <?php $subcategoria = json_decode($value->SUBCATEGORIA)?>
+
+            <div class="accordion" id="accordionExample">
+          <?php foreach ($subcategoria as $sub) : ?>
+
+            <?php if(isset($sub->PRODUCTOS)):?>
+          <div class="card" style="background-color: rgba(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?>, 0.6 )">
+          <div class="card-header" id="heading_sc_<?=$sub->ID_SUB_CATEGORIA_1?>">
+          <h6>
+          <?=$sub->SUB_CATEGORIA_1?>
+            <button class="btn btn-danger float-right btn-xs" data-toggle="collapse" data-target="#subcollapse<?=$sub->ID_SUB_CATEGORIA_1?>" aria-expanded="true" aria-controls="collapseOne">
             <i class="las la-plus" ></i>
-          </button>
-        </h5>
-      </div>
+            </button>
+          </h6>
+          </div>
 
-      <div id="collapse<?=$value->ID_CATEGORIA?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-        <div class="card-body" style="background-color:white">
-
-        <?php $subcategoria = json_decode($value->SUBCATEGORIA)?>
-
-        <div class="accordion" id="accordionExample">
-			<?php foreach ($subcategoria as $sub) : ?>
-			<div class="card" style="background-color: rgba(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?>, 0.5 )">
-			<div class="card-header" id="heading_sc_<?=$sub->ID_SUB_CATEGORIA_1?>">
-			<h5 class="mb-0">
-			<?=$sub->SUB_CATEGORIA_1?>
-				<button class="btn btn-danger float-right btn-xs" data-toggle="collapse" data-target="#subcollapse<?=$sub->ID_SUB_CATEGORIA_1?>" aria-expanded="true" aria-controls="collapseOne">
-				<i class="las la-plus" ></i>
-				</button>
-			</h5>
-			</div>
-
-				<div id="subcollapse<?=$sub->ID_SUB_CATEGORIA_1?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-				<div class="card-body" style="background-color:white">
-					Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
-				</div>
-				</div>
-			</div>
-			<?php endforeach; ?>
-		</div>
-
-           
+            <div id="subcollapse<?=$sub->ID_SUB_CATEGORIA_1?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body" style="background-color:white">
+                <table class="table-classic" id="table_<?=$sub->ID_SUB_CATEGORIA_1?>">
+                      <th style="background-color: rgba(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?>, 0.4 )">Producto</th>
+                      <th style="background-color: rgba(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?>, 0.4 )">Unidad Medida</th>
+                      <th style="background-color: rgba(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?>, 0.4 )">Cantidad Declarada</th>
+                      <?php foreach ($sub->PRODUCTOS as $p): ?>
+                        <tr>
+                            <td width="40%"><?=$p->SUB_CATEGORIA_2?></td>
+                            <td width="30%">
+                              <?=$p->MEDIDA_ESTANDARIZACION?>                          
+                            </td>
+                            <td width="30%">
+                              <input name="p_<?=$p->ID_SUB_CATEGORIA_2?>" class="form-control" type="number" min="0" step="0.1">
+                            </td>
+                        </tr>
+                      <?php endforeach; ?>
+                </table>
+            </div>
+            </div>
+          </div>
+          <?php endif; ?>
+          <?php endforeach; ?>
         </div>
-      </div>
+            </div>
+          </div>
+        </div>
+      <?php endforeach; ?>
     </div>
-  <?php endforeach; ?>
+  </form>
 </div>
 
 <script>
-    $('.collapse').collapse()
+    $('.collapse').collapse();
+
+
+    function guardarDeclaracion(){
+
+      var collection = $('#serializeExample form').serialize();
+
+      console.log(collection);
+
+    }
 </script>
-
-
-
-
-
-
-
-<!--
-<div id="accordion">
-  <?php foreach ($existencia as $value) : ?>
-    <div class="card" style="background-color: rgb(<?=$value->COLOR_R?>, <?=$value->COLOR_G?>, <?=$value->COLOR_B?> )">
-      <div class="card-header" id="heading<?=$value->ID_CATEGORIA?>">
-        <h5 class="mb-0">
-        <?=$value->CATEGORIA?>
-
-          <button class="btn btn-danger float-right btn-xs" data-toggle="collapse" data-target="#collapse<?=$value->ID_CATEGORIA?>" aria-expanded="true" aria-controls="collapseOne">
-            <i class="las la-plus" ></i>
-          </button>
-        </h5>
-      </div>
-
-      <div id="collapse<?=$value->ID_CATEGORIA?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-        <div class="card-body" style="background-color:white">
-            <table id="table_<?=$value->ID_CATEGORIA?>">
-                           
-            </table>
-          
-            <script>
-              var table = $('#table_<?=$value->ID_CATEGORIA?>').DataTable({
-                  responsive: true,
-                  data: '<?=$value->PRODUCTOS?>',
-                  language:{ search: "Buscar", lengthMenu: "Mostrar _MENU_", previous: "Anterior",
-                  zeroRecords: "Sin resultados", infoEmpty: "No hay registros disponibles",
-                  info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                  infoFiltered: "(Filtrado de _MAX_ registros totales)",
-                  oPaginate: {sNext:"Siguiente", sLast: "Último", sPrevious: "Anterior", sFirst:"Primero" },
-                  },
-                  columns: [
-                      { title: 'N°', width:'10%',data: 'row' },
-                      { title: 'Producto', width:'10%' ,data: 'PRODUCTO_MADRE' },
-                      { title: 'Sub-categoria', width:'10%' ,data: 'CATEGORIA_2' },
-                      { title: 'Categoria', width:'10%' ,data: 'CATEGORIA' },
-                      { title: 'Precio', width:'10%' ,data: 'PRECIO' },
-                      { title: 'Lista', width:'10%' ,data: 'NOMBRE_LISTA_PRECIOS' },
-                  ],
-              });
-            </script>
-        </div>
-      </div>
-    </div>
-  <?php endforeach; ?>
-</div>
-
-
-<!--
-
-<table id="table_<?=$value->ID_CATEGORIA?>">
-                           
-                           </table>
-                         
-                           <script>
-                             var table = $('#table_<?=$value->ID_CATEGORIA?>').DataTable({
-                                 responsive: true,
-                                 data: '<?=$value->PRODUCTOS?>',
-                                 language:{ search: "Buscar", lengthMenu: "Mostrar _MENU_", previous: "Anterior",
-                                 zeroRecords: "Sin resultados", infoEmpty: "No hay registros disponibles",
-                                 info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                                 infoFiltered: "(Filtrado de _MAX_ registros totales)",
-                                 oPaginate: {sNext:"Siguiente", sLast: "Último", sPrevious: "Anterior", sFirst:"Primero" },
-                                 },
-                                 columns: [
-                                     { title: 'N°', width:'10%',data: 'row' },
-                                     { title: 'Producto', width:'10%' ,data: 'PRODUCTO_MADRE' },
-                                     { title: 'Sub-categoria', width:'10%' ,data: 'CATEGORIA_2' },
-                                     { title: 'Categoria', width:'10%' ,data: 'CATEGORIA' },
-                                     { title: 'Precio', width:'10%' ,data: 'PRECIO' },
-                                     { title: 'Lista', width:'10%' ,data: 'NOMBRE_LISTA_PRECIOS' },
-                                 ],
-                             });
-                           </script>
-                            -->
